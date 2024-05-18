1bb8360b7a18b2ccd009d94bfb26a5ff43aca3ac
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-05-18 19:41:25 +0200

    removed fas and checkbutton

--- src/Marker.php
+++ src/Marker.php
@@ -12,10 +12,10 @@ namespace HexMakina\Marker;
 class Marker extends Element
 {
     /**
-      * ? makes more sense to write
+      * ? smoother write
       * Marker::img('path/to/img.jpg', 'An alternative text', ['width' => 34, 'height' => 34])
       * than
-      * Marker::img(null, ['src' => 'path/to/img.jpg', 'alt' => 'An alternative text', 'width' => 34, 'height' => 34])
+      * Element::img(null, ['src' => 'path/to/img.jpg', 'alt' => 'An alternative text', 'width' => 34, 'height' => 34])
       */
     public static function img(string $src, string $alt, array $attributes = []): Element
     {
@@ -37,37 +37,5 @@ class Marker extends Element
         $attributes['href'] ??= $href;
 
         return new Element('a', $label, $attributes);
-    }
-
-    public static function fas(string $icon, string $title = null, array $attributes = []): Element
-    {
-        $attributes['title'] ??= $title; // attributes take precedence
-        $attributes['class'] = sprintf('fas fa-%s %s', $icon, $attributes['class'] ?? '');
-        return new Element('i', '', $attributes);
-    }
-
-    public static function checkbutton(string $name, mixed $value, string $label, array $attributes = []): Element
-    {
-        if (!isset($attributes['id'])) {
-            $attributes['id'] = $name;
-        }
-
-        if (!isset($attributes['type'])) {
-            $attributes['type'] = 'checkbox'; // default
-        }
-
-        if (isset($attributes['is_checked']) && $attributes['is_checked'] === true) { // for boolean checkbuttons
-            $attributes['checked'] = 'checked';
-            unset($attributes['is_checked']);
-        }
-
-        return
-        Marker::div(
-            Marker::label(
-                Form::input($name, $value, $attributes) . Marker::span($label),
-                ['for' => $attributes['id']]
-            ),
-            ['class' => 'checkbutton']
-        );
     }
 }
