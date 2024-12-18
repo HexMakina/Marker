3a95afb9f0b686c5f64aa38a2aaf310a079250ab
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-12-18 02:47:28 +0100

    __unset() instead of __set() with empty value + __isset + doc update

--- src/Element.php
+++ src/Element.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace HexMakina\Marker;
 
 /**
- * The Element class provides a simple and useful implementation for creating,  and generating valid HTML elements using PHP
+ * The Element class provides a simple and useful implementation for creating, and generating valid HTML elements using PHP
  * The __toString() method of the Element class generates the HTML code for the element based on its tag, attributes, and inner content
  *
  *
@@ -42,23 +42,24 @@ class Element
     protected array $attributes = [];
 
     // Constants for sprintf formats
-    public const FORMAT_TAG_VOID = '<%s%s/>';
-    public const FORMAT_TAG = '<%s%s>%s</%s>';
-    public const FORMAT_ATTRIBUTE = ' %s="%s"';
+    public const FORMAT_TAG_VOID = '<%s%s/>';   // <br/>, <img src="path/to/jpeg.png" alt="hyper descriptive" width="100" height="100"/>
+    public const FORMAT_TAG = '<%s%s>%s</%s>';  // <span class="bar">foo</span>, <div class="bar">foo</div>
+    public const FORMAT_ATTRIBUTE = ' %s="%s"'; // id="publication", class="container", data-toggle="modal"
 
     /**
      * Creates a new Element instance using a static method call.
      *
-     * ex: Element::span('foo', ['class' => 'bar'])
+     * Element::span('foo', ['class' => 'bar']) gives the same result as 
+     * new Element('span', 'foo', ['class' => 'bar'])
+     * 
      * ex: Element::div('foo', ['class' => 'bar'])
-     * ex: Element::img(null, ['src' => 'path/to/jpeg.png', alt='hyper descriptive', 'width' => 100, 'height' => 100])
+     * ex: Element::img(null, ['src' => 'path/to/jpeg.png', 'alt' => 'hyper descriptive', 'width' => 100, 'height' => 100])
      * ex: Element::a('click here', ['href' => 'url/to/destination'])
 
      * @param string $tag The HTML tag for the element.
-     * @param mixed $content The content of the element.
+     * @param mixed $content The content of the element, null for void elements.
      * @param array $attributes The attributes of the element.
      * @return Element The newly created Element instance.
-     * 
      */
     public static function __callStatic(string $tag, array $arguments): self
     {
@@ -72,11 +73,11 @@ class Element
      * A null $inner means that the HTML element is a void element, ie br, hr, img, etc.
      * ex: new Element('span', 'foo', ['class' => 'bar'])
      * ex: new Element('div', 'foo', ['class' => 'bar'])
-     * ex: new Element('img', null, ['src' => 'path/to/jpeg.png', alt='hyper descriptive', 'width' => 100, 'height' => 100])
+     * ex: new Element('img', null, ['src' => 'path/to/jpeg.png', 'alt'='hyper descriptive', 'width' => 100, 'height' => 100])
      * ex: new Element('a', 'click here', ['href' => 'url/to/destination'])
      * 
      * @param string $tag HTML tag, ie span, div, p, etc.
-     * @param string|null $inner what will be inside the tag (null means void element)
+     * @param ?string $inner what will be inside the tag (null means void element)
      * @param mixed[] $attributes An array of attributes for the element (optional).
      * 
      */
@@ -90,7 +91,6 @@ class Element
             if (is_int($name)) {
                 $name = $value;
             }
-
             $this->$name = $value;
         }
     }
@@ -107,11 +107,6 @@ class Element
      */
     public function __set(string $name, $value = null)
     {
-        if (empty($value) && $value !== '0') {
-            unset($this->attributes[$name]);
-            return;
-        }
-
         $this->attributes[$name] = is_array($value) ? implode(' ', $value) : $value;
     }
 
@@ -122,12 +117,35 @@ class Element
      * Empty string because the class aims for ease of use, *not* strictness, for once.     
      *
      * @param string $name The name of the attribute.
-     * @return string|null The value of the attribute, or '' if it doesn't exist.
+     * @return string The value of the attribute, or '' if it doesn't exist.
      * 
      */
     public function __get(string $name)
     {
         return $this->attributes[$name] ?? '';
+    }
+
+    /**
+     * Magic method to check if an attribute exists.
+     *
+     * @param string $name The name of the attribute.
+     * @return bool True if the attribute exists, false otherwise.
+     * 
+     */
+    public function __isset(string $name): bool
+    {
+        return isset($this->attributes[$name]);
+    }
+
+    /**
+     * Magic method to unset an attribute.
+     *
+     * @param string $name The name of the attribute.
+     * 
+     */
+    public function __unset(string $name)
+    {
+        unset($this->attributes[$name]);
     }
 
     /**
@@ -145,7 +163,7 @@ class Element
             $attributes .= sprintf(self::FORMAT_ATTRIBUTE, $name, $value);
         }
 
-        return is_null($this->inner)
+        return $this->inner === null
             ? sprintf(self::FORMAT_TAG_VOID, $this->tag, $attributes)
             : sprintf(self::FORMAT_TAG, $this->tag, $attributes, $this->inner, $this->tag);
     }
